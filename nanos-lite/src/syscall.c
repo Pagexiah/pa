d10f86e3adced7d9e64f24af3c3a048119093398
tracer-ics2023 <tracer@njuics.org>
2024-01-18 18:02:58 +0800

    >  compile NEMU 221300029 夏艺匀 Linux pagexia-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:02:58 up  2:48,  1 user,  load average: 0.01, 0.32, 0.33

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,17 @@
 #include <common.h>
 #include "syscall.h"
+
+void sys_write(int fd,void* buf, size_t count){
+  char *buff=(char *)buf;
+  if(fd==1 ||fd==2){
+    int i=0;
+    while(buff[i]!='\0' && i<count){
+      putch(buff[i]);
+      i++;
+    }
+  }
+  //return i;
+}
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -7,7 +19,7 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case 1: printf("SYS_yield\n");c->GPRx=0;yield();break;
     case 0:printf("SYS_exit\n");c->GPRx=0;halt(c->GPR2);break; 
-    case 4:printf("SYS_write\n");break;
+    case 4:printf("SYS_write\n");sys_write((int)c->GPR2,(void *)c->GPR3,(size_t)c->GPR4);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
