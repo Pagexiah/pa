a36762e0ddc581fcd6b6270f3cfbf24d20389978
tracer-ics2023 <tracer@njuics.org>
2024-01-19 19:24:21 +0800

    >  compile NEMU 221300029 夏艺匀 Linux pagexia-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:24:21 up  2:31,  1 user,  load average: 0.12, 0.16, 0.17

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include<sys/time.h>
+/*
 int main() {
   struct timeval old;
   assert(gettimeofday(&old,NULL)==0);
@@ -10,6 +11,19 @@ int main() {
    assert(gettimeofday(&cur,NULL)==0);
    uint64_t gap=(cur.tv_sec-old.tv_sec)*1000000+(cur.tv_usec-old.tv_usec);
    if(gap>500000*loop){
+     printf("Hello %d times\n",loop);
+     loop++;
+   }
+  }
+  return 0;
+}
+*/
+int main() {
+  uint64_t old=NDL_GetTicks()*1000;
+  int loop=1;
+  while (1) {
+   uint64_t gap=NDL_GetTicks()*1000-old;
+   if(gap>5*loop){
      printf("Hello %d times\n",loop);
      loop++;
    }
