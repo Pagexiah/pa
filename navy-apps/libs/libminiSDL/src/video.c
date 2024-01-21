0c3fc0e4240e039d13b6afec7f903de6d8247d61
tracer-ics2023 <tracer@njuics.org>
2024-01-21 21:22:32 +0800

    >  compile NEMU 221300029 夏艺匀 Linux pagexia-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:32 up 20:16,  1 user,  load average: 1.33, 0.88, 0.79

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -133,7 +133,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
      int H=x==0&&y==0&&w==0&&h==0?s->h:h;
      printf("W %d H %d s->w %d s->h %d w %d h %d\n",W,H,s->w,s->h,w,h);
      printf("W*H %d\n",W*H);
-        uint32_t * pixels = (uint32_t *)malloc(W * H * sizeof(uint32_t *));
+        uint32_t  pixels[W*H];// = (uint32_t *)malloc(W * H * sizeof(uint32_t *));
         for (int i = 0; i < W*H; i++){
             SDL_Color color = s->format->palette->colors[((uint8_t*)(s->pixels))[i]];
             pixels[i] = ((uint32_t)color.a << 24) | ((uint32_t)color.r << 16) | ((uint32_t)color.g << 8) | ((uint32_t)color.b << 0);
@@ -141,7 +141,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
         }
         printf("here\n");
         NDL_DrawRect(pixels, x, y, W, H);
-        free(pixels);
+        //free(pixels);
   }
   else{
     if(x==0 &&y==0 &&w==0&&h==0){
